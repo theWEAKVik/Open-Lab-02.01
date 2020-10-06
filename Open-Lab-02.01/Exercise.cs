a4669854f636384078c2c89fc2c4bee19118c3ce
Viktor <krpectv57@gmail.com>
2020-10-06 11:14:44 +0200

    double

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -6,12 +6,13 @@ namespace Open_Lab_02._01
     {
         public double[] Create5Numbers()
         {
-            throw new NotImplementedException();
+           double[] nastraz = new double[5];
+            return nastraz;
         }
 
         public double GetSecond(double[] numbers)
         {
-            throw new NotImplementedException();
+           return numbers[1];
         }
     }
 }
